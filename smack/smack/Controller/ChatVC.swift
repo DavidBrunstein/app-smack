4216420292ea718895cf393deb28918ae7d3e458
Davo Brunstein <davidbrunstein@gmail.com>
2017-12-27 18:35:52 -0600

    SW Reveal View Controller working

---
+++ smack/smack/Controller/ChatVC.swift
@@ -0,0 +1,25 @@
+//
+//  ChatVC.swift
+//  smack
+//
+//  Created by David Brunstein on 2017-12-27.
+//  Copyright © 2017 David Brunstein. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+
+    // Outlets
+    @IBOutlet weak var menuBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+    }
+
+
+}
