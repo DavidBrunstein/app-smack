d5c86c6ea92919f2f4f14d5ab4b3218557fa9e03
Davo Brunstein <davidbrunstein@gmail.com>
2018-04-05 20:06:38 -0500

    Login page

---
+++ smack/smack/Controller/LoginVC.swift
@@ -0,0 +1,24 @@
+//
+//  LoginVC.swift
+//  smack
+//
+//  Created by David Brunstein on 2018-04-05.
+//  Copyright © 2018 David Brunstein. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func closeBtnPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
---
+++ smack/smack/Utilities/Contants.swift
@@ -0,0 +1,14 @@
+//
+//  Contants.swift
+//  smack
+//
+//  Created by David Brunstein on 2017-12-29.
+//  Copyright © 2017 David Brunstein. All rights reserved.
+//
+
+import Foundation
+
+
+// Segues
+let TO_LOGIN = "toLogin"
+
